5291ec2dbf3d6a1feba29c8fd10e150d7c45bc14
shrey <117995509+sane-being@users.noreply.github.com>
2025-03-08 12:39:48 +0000

    Post: generate scaffold & add validations
    - generated scaffold for Post table
    bin/rails generate scaffold Post body:text
    
    - added validations for Post's columns at
    app/models/post.rb
    validates :body, presence: true

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_08_123126) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_08_123559) do
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
