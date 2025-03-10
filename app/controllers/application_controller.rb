c6258d896747a146e16b7382c43fdc52cc82d021
shrey <117995509+sane-being@users.noreply.github.com>
2025-03-10 07:01:11 +0000

    add username attribute to User
    - Add username column to User
    bin/rails generate migration AddUsernameToUsers username:string
    rails db:migrate
    
    - Allow controller to accept username in params by editing:
    app/controllers/users/registrations_controller.rb
    Add username as attribute in methods:
    configure_sign_up_params & configure_account_update_params
    
    - configure routes to use this custom controller instead of default
    devise controller
    in config/routes.rb:
    devise_for :users, controllers: { registrations: "users/registrations" }
    
    - add validation for username in app/models/user.rb
    validates :username, uniqueness: true
    
    - add username field to sign up and account update views
    app/views/users/registrations/
    
    - edit views: instead of email, username is now shown with post when
    signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # # Permit to take username on sign up page
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # protected
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [ :username ])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [ :username ])
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_08_124534) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_10_063629) do
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_08_124534) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
